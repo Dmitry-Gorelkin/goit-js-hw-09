f116fbc2bc8ccb56fadba3b52ebeffc5eb141234
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-01-09 23:04:09 +0200

    add task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+import { getRandomHexColor } from './random-color';
 
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+
+const elementStart = refs.start;
+const elementStop = refs.stop;
+let timerId = null;
+
+function onStart(e) {
+  if (e.target.dataset.start !== '') return;
+
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  elementStart.setAttribute('disabled', 'disabled');
+  elementStop.removeAttribute('disabled');
+}
+
+function onStop(e) {
+  if (e.target.dataset.stop !== '') return;
+
+  clearInterval(timerId);
+
+  elementStart.removeAttribute('disabled');
+  elementStop.setAttribute('disabled', 'disabled');
+}
+
+elementStop.setAttribute('disabled', 'disabled');
+refs.start = addEventListener('click', onStart);
+refs.stop = addEventListener('click', onStop);
